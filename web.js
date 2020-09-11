04fd057ba82a7bf39675b90dbf35910f55ca2ecd
Paska Choothong <n_namobwang@hotmail.com>
2020-09-11 14:58:35 +0700

    restructure randommap, executiontime , cpu usage

--- web.js
+++ web.js
@@ -1,6 +1,5 @@
 const {map} = require('./map.js'); 
 const {ids} = require('./iterativeDeepeningSearch.js');
 const {unc} = require('./uniformCost.js');
-console.log("map1 is: "+map[0]);
 unc(map[3])
 //ids(map[0]);
