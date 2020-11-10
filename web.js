7f2344202b7e51dcfd5225987f382826191d4078
Chatrudee <60010173@kmitl.ac.th>
2020-11-10 13:55:28 +0700

    edit delay

--- web.js
+++ web.js
@@ -1,8 +1,9 @@
 const {map} = require('./map.js'); 
 // const {ids} = require('./iterativeDeepeningSearch.js');
-// const {unc} = require('./uniformCost.js');
+ const {unc} = require('./uniformCost.js');
 // const {biunc} = require('./bidirectionalUniformCost.js');
-const {aStarSearch} = require('./aStartSearch.js')
+//const {aStarSearch} = require('./aStartSearch.js')
 console.log("map1 is: "+map[0]);
-aStarSearch(map[3]);
+unc(map[0]);
+//aStarSearch(map[0]);
 //ids(map[0]);
